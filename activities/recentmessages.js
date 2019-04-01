055dc87be0b074583b631f51ef95727489e377f7
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-04-01 09:18:52 +0200

    Code refactoring and optimization.

--- activities/recentmessages.js
+++ activities/recentmessages.js
@@ -4,7 +4,6 @@ const api = require('./common/api');
 module.exports = async function (activity) {
   try {
     const roomsResponse = await api(`/rooms`);
-
     if (Activity.isErrorResponse(roomsResponse)) return;
 
     let rooms = roomsResponse.body.items;
@@ -12,19 +11,31 @@ module.exports = async function (activity) {
     let messages = [];
     let mentions = [];
     let files = [];
+
+    let promises = [];
+    let results = [];
+
     for (let i = 0; i < rooms.length; i++) {
-      const messagesResponse = await api(`/messages?roomId=${rooms[i].id}`);
-      if (Activity.isErrorResponse(messagesResponse)) return;
-      for (let j = 0; j < messagesResponse.body.items.length; j++) {
-        let raw = messagesResponse.body.items[j];
+      promises.push(api(`/messages?roomId=${rooms[i].id}`));
+    }
+
+    results = await Promise.all(promises);
+
+    for (let i = 0; i < results.length; i++) {
+      if (Activity.isErrorResponse(results[i])) return;
+
+      for (let j = 0; j < results[i].body.items.length; j++) {
+        let raw = results[i].body.items[j];
         let item = { id: raw.id, title: raw.roomType, description: raw.text, link: raw.url, raw: raw };
         messages.push(item);
+
         if (raw.files) {
           for (let z = 0; z < raw.files.length; z++) {
             let file = raw.files[z];
             files.push(file);
           }
         }
+
         if (raw.mentionedPeople) {
           for (let z = 0; z < raw.mentionedPeople.length; z++) {
             let mention = raw.mentionedPeople[z];
