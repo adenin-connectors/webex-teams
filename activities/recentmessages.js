df7dd6d41609d68560cb4701b2249830e5762cfb
olliejm <olliejm@protonmail.com>
2019-04-25 11:03:35 +0100

    refactoring

--- activities/recentmessages.js
+++ activities/recentmessages.js
@@ -13,34 +13,34 @@ module.exports = async (activity) => {
   try {
     api.initialize(activity);
 
-    const roomsResponse = await api('/rooms');
+    const rooms = await api('/rooms');
 
-    if ($.isErrorResponse(activity, roomsResponse)) return;
+    if ($.isErrorResponse(activity, rooms)) return;
 
     // groups api requests and sends them in parallel
-    const messages = [];
+    const messagePromises = [];
 
-    for (let i = 0; i < roomsResponse.body.items.length; i++) {
-      messages.push(api(`/messages?roomId=${roomsResponse.body.items[i].id}`));
+    for (let i = 0; i < rooms.body.items.length; i++) {
+      messagePromises.push(api(`/messages?roomId=${rooms.body.items[i].id}`));
     }
 
-    const messageResults = await Promise.all(messages);
+    const messages = await Promise.all(messagePromises);
 
     //filters messages by date and time they were sent
-    const filteredMessageResults = [];
+    const filteredMessages = [];
 
-    for (let i = 0; i < messageResults.length; i++) {
-      if ($.isErrorResponse(activity, messageResults[i])) return;
+    for (let i = 0; i < messages.length; i++) {
+      if ($.isErrorResponse(activity, messages[i])) return;
 
-      filteredMessageResults.push(filterMessagesByTime(messageResults[i].body.items));
+      filteredMessages.push(filterMessagesByTime(messages[i].body.items));
       //filteredMessageResults.push(messageResults[i].body.items); // for testing, if recent items is empty or too small
     }
 
     const me = await api('/people/me');
 
     //converts messages to items and filters out mentions and files
-    const users = new Map();
-    //const files = []; // disable files for now
+    const userPromises = new Map();
+    //const rawFiles = []; // disable files for now
 
     const data = {
       messages: {
@@ -54,9 +54,9 @@ module.exports = async (activity) => {
       }
     };
 
-    for (let i = 0; i < filteredMessageResults.length; i++) {
-      for (let j = 0; j < filteredMessageResults[i].length; j++) {
-        const raw = filteredMessageResults[i][j];
+    for (let i = 0; i < filteredMessages.length; i++) {
+      for (let j = 0; j < filteredMessages[i].length; j++) {
+        const raw = filteredMessages[i][j];
         const item = {
           id: raw.id,
           title: raw.roomType,
@@ -67,20 +67,20 @@ module.exports = async (activity) => {
         };
 
         // get room name for the message
-        for (let k = 0; k < roomsResponse.body.items.length; k++) {
-          if (raw.roomId === roomsResponse.body.items[k].id) item.room = roomsResponse.body.items[k].title;
+        for (let k = 0; k < rooms.body.items.length; k++) {
+          if (raw.roomId === rooms.body.items[k].id) item.room = rooms.body.items[k].title;
         }
 
         // push constructed item
         data.messages.items.push(item);
 
         // if we haven't encountered this user yet, store promise to retrieve user data in map
-        if (!users.has(raw.personId)) users.set(raw.personId, api(`/people/${raw.personId}`));
+        if (!userPromises.has(raw.personId)) userPromises.set(raw.personId, api(`/people/${raw.personId}`));
 
         //checks for files, store promise to get info as well as author and date
         /*if (raw.files) {
           for (let k = 0; k < raw.files.length; k++) {
-            files.push({
+            rawFiles.push({
               promise: api.head(raw.files[k]),
               personId: raw.personId,
               created: raw.created,
@@ -98,47 +98,47 @@ module.exports = async (activity) => {
       }
     }
 
-    const userResults = await Promise.all(users.values());
+    const users = await Promise.all(userPromises.values());
 
     // Loop through user info for all users
-    for (let i = 0; i < userResults.length; i++) {
-      if ($.isErrorResponse(activity, userResults[i])) return;
+    for (let i = 0; i < users.length; i++) {
+      if ($.isErrorResponse(activity, users[i])) return;
 
       // map extended user info onto matching messages
       for (let j = 0; j < data.messages.items.length; j++) {
-        if (data.messages.items[j].raw.personId === userResults[i].body.id) {
-          data.messages.items[j].displayName = userResults[i].body.displayName;
-          data.messages.items[j].avatar = userResults[i].body.avatar;
+        if (data.messages.items[j].raw.personId === users[i].body.id) {
+          data.messages.items[j].displayName = users[i].body.displayName;
+          data.messages.items[j].avatar = users[i].body.avatar;
         }
       }
 
       // map extended user info onto matching mentions
       for (let j = 0; j < data.mentions.items.length; j++) {
-        if (data.messages.items[j].raw.personId === userResults[i].body.id) {
-          data.mentions.items[j].displayName = userResults[i].body.displayName;
-          data.mentions.items[j].avatar = userResults[i].body.avatar;
+        if (data.messages.items[j].raw.personId === users[i].body.id) {
+          data.mentions.items[j].displayName = users[i].body.displayName;
+          data.mentions.items[j].avatar = users[i].body.avatar;
         }
       }
 
       // get correct user name to display with file info
-      /*for (let j = 0; j < files.length; j++) {
-        if (files[j].personId === userResults[i].body.id) files[j].displayName = userResults[i].body.displayName;
+      /*for (let j = 0; j < rawFiles.length; j++) {
+        if (rawFiles[j].personId === users[i].body.id) rawFiles[j].displayName = users[i].body.displayName;
       }*/
     }
 
     // await file promises to get type and filename
-    /*const fileResults = await Promise.all(files.map(async (file) => file.promise));
+    /*const files = await Promise.all(rawFiles.map(async (file) => file.promise));
 
-    for (let i = 0; i < fileResults.length; i++) {
-      if (Activity.isErrorResponse(fileResults[i])) return;
+    for (let i = 0; i < files.length; i++) {
+      if ($.isErrorResponse(activity, files[i])) return;
 
-      const disposition = fileResults[i].headers['content-disposition'];
+      const disposition = files[i].headers['content-disposition'];
 
       data.files.items.push({
-        type: fileResults[i].headers['content-type'],
+        type: files[i].headers['content-type'],
         name: disposition.substring(disposition.indexOf('"') + 1, disposition.lastIndexOf('"')),
-        author: files[i].displayName,
-        created: files[i].created
+        author: rawFiles[i].displayName,
+        created: rawFiles[i].created
       });
     }*/
 
